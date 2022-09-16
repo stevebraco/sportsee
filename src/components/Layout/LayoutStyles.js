876c78b451923f015766abd6b679a392eb93200d
steve <steve.braco@gmail.com>
2022-09-16 10:39:57 +0200

    add height

--- src/components/Layout/LayoutStyles.js
+++ src/components/Layout/LayoutStyles.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const ContainerFlex = styled.div`
   display: flex;
   align-items: stretch;
+  height: calc(780px - 91px);
 `;
 
 export const Main = styled.div`
